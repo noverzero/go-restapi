cb50f6740692057238e970cbd1bdf87092e90f2f
dustinhuth <dustin@undefinedindustries.com>
2019-11-21 01:06:30 -0700

    update working

--- main.go
+++ main.go
@@ -66,7 +66,12 @@ func updateBook(w http.ResponseWriter, r *http.Request){
 	for index, item := range books {
 		if item.ID == params["id"] {
 			books = append(books[:index], books[index+1:]...)
-			break
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
 		}
 	}
 	json.NewEncoder(w).Encode(books)
